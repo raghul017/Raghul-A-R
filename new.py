93041ac87659c087fc9ab82904b727ac91b23e4e
raghul017 <113668235+raghul017@users.noreply.github.com>
2022-09-16 14:58:58 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,21 @@
+l1=["bulbbasoor_21","coocoo_52","squirtle_93","greninja45_23","gengar_02"]
+#l2=l1[::-1]
+#for i in l2:
+ #   print(i[-4::-1])
+
+"""for i in l1:
+    x=i.startswith("g")
+    if x==True:
+        l2=i.find("_")
+        print(i[:l2])
+for i in l1:
+    if i[0]=="g":
+        k=i.find("_")
+        print(i[:k])
+    else:
+        continue"""
+    
+l2=[]
+for i in l1[::2]:
+    l2.append(i)
+print(l24W#Wq)
